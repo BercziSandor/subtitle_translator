f3678d3206a0ef426bec2036e64b06eb392aaa9b
BercziSandor <berczi.sandor@gmail.com>
2022-02-23 08:46:49 +0100

    mypy fixes 1

--- src/subtitle_translator/main.py
+++ src/subtitle_translator/main.py
@@ -1,5 +1,6 @@
-import copy
-import datetime
+"""
+TODO
+"""
 import io
 import logging
 import re
@@ -30,33 +31,11 @@ sample_str = io.StringIO(textwrap.dedent('''\
 '''))
 
 
-def subtitle_parser_test():
-    def get_subtitles_from_file(inp: str):
-        import subtitle_parser
-        with open(inp, 'r', encoding='utf-8') as input_file:
-            parser = subtitle_parser.SrtParser(input_file)
-            parser.parse()
-            parser.print_warnings()
-
-        return parser.subtitles
-
-    def get_subtitles_from_str(inp: str):
-        from subtitle_parser import SrtParser
-        parser = SrtParser(inp)
-        parser.parse()
-        parser.print_warnings()
-
-        return parser.subtitles
-
-    subtitles = get_subtitles_from_str(sample_str)
-    subtitles = get_subtitles_from_file(sample_file)
-    texts = [subtitle.text for subtitle in subtitles]
-    translated = GoogleTranslator(source='german', target='english').translate_batch(texts)
-
-
 def translate_array(texts, source_language='auto', target_language='hu'):
-    for i in range(len(texts)):
-        text = texts[i]
+    """
+        TODO
+    """
+    for i, text in enumerate(texts):
         if not text or not isinstance(text, str) or not text.strip():
             texts[i] = " zzz "
 
@@ -67,72 +46,72 @@ def translate_array(texts, source_language='auto', target_language='hu'):
     return result
 
 
-def split_up(text: str, pieces: int = 2) -> List[str]:
-    ps = []
-    if pieces == 1:
-        return [text]
-    elif pieces < 1:
+def split_up(text: str, pieces_count: int = 2) -> List[str]:
+    """
+    TODO
+    """
+    pieces = []
+
+    if pieces_count < 1:
         logging.error("pieces error.")
-        exit(1)
+        sys.exit(1)
+    elif pieces_count == 1:
+        return [text]
 
     def get_optimal_split(where: float, p_split_points: List[int]):
-        minnn = 9999.0
+        distance_min = 9999.0
         for a_split_point in p_split_points:
-            d = abs(where - a_split_point)
-            if d < minnn:
-                minnn = d
+            distance = abs(where - a_split_point)
+            if distance < distance_min:
+                distance_min = distance
                 min_point = a_split_point
 
         p_split_points.remove(min_point)
         return min_point, p_split_points
 
-    len_of_a_piece = len(text) / pieces
-    optimal_split_positions = [len_of_a_piece * x for x in range(1, pieces)]
+    len_of_a_piece = len(text) / pieces_count
+    optimal_split_positions = [len_of_a_piece * x for x in range(1, pieces_count)]
     indices_object = re.finditer(pattern=r'\w+', string=text)
 
     possible_split_points = [index.start() for index in indices_object]
-    if 0 in possible_split_points: possible_split_points.remove(0)
+    if 0 in possible_split_points:
+        possible_split_points.remove(0)
 
-    if len(possible_split_points) + 1 < pieces:
-        p = re.split(r'\W+', text)
-        p.remove('')
-        logging.info("[{}]".format(" | ".join(p)))
+    if len(possible_split_points) + 1 < pieces_count:
+        logging.info("[{}]".format(" | ".join(re.split(r'\W+', text).remove(''))))
         logging.error(
-            f"There are {len(possible_split_points)} split points and we want to split the text '{text}' in {pieces} "
-            f"pieces... Giving up.")
+            f"There are {len(possible_split_points)} split points and we want "
+            f"to split the text '{text}' in {pieces_count} pieces... Giving up.")
         sys.exit(42)
 
     def get_split_points(optimal_split_positions: List[float],
                          p_possible_split_points: List[int] = possible_split_points):
         split_points = []
-        for o in optimal_split_positions:
-            sp, p_possible_split_points = get_optimal_split(where=o, p_split_points=p_possible_split_points)
-            split_points.append(sp)
+        for an_optimal_position in optimal_split_positions:
+            a_split_point, p_possible_split_points = get_optimal_split(where=an_optimal_position,
+                                                                       p_split_points=p_possible_split_points)
+            split_points.append(a_split_point)
         return split_points
 
     start_ind = 0
     for split_point in get_split_points(optimal_split_positions=optimal_split_positions,
                                         p_possible_split_points=possible_split_points):
-        ps.append(text[start_ind:split_point].strip())
+        pieces.append(text[start_ind:split_point].strip())
         start_ind = split_point
-    ps.append(text[start_ind:].strip())
+    pieces.append(text[start_ind:].strip())
 
-    logging.debug(f"Splitting up '{text}' in {pieces} pieces: {ps}")
+    logging.debug(f"Splitting up '{text}' in {pieces_count} pieces: {pieces}")
 
-    return ps
-
-
-def strfdelta(tdelta, fmt):
-    d = {"days": tdelta.days}
-    d["hours"], rem = divmod(tdelta.seconds, 3600)
-    d["minutes"], d["seconds"] = divmod(rem, 60)
-    return fmt.format(**d)
+    return pieces
 
 
-def translate_subtitle_file(input=sample_file, target_language='hu'):
+def translate_subtitle_file(input_file=sample_file, target_language='hu'):
+    """
+    TODO
+    """
     translation_char_limit = 4000  # 4000
-    subtitle = sublib.SubRip(input, "utf-8")
-    s2 = copy.deepcopy(subtitle)
+    subtitle = sublib.SubRip(input_file, "utf-8")
+    # s2 = copy.deepcopy(subtitle)
     general = subtitle.get_general_format()
 
     def is_end_of_sentence(text: str):
@@ -148,8 +127,8 @@ def translate_subtitle_file(input=sample_file, target_language='hu'):
     entry = {'index_start': 0, 'index_end': 0, 'text': ''}
 
     logging.info("# Phase 1: Prepare translation: Join entries to sentences.")
-    for i in range(len(general)):
-        start, end, text = general[i]
+    for i, a_general in enumerate(general):
+        start, end, text = a_general
         text = text.replace('|', ' ').replace('  ', '')
         if len(entry['text']) > 0:
             entry['text'] += ' '
@@ -172,27 +151,28 @@ def translate_subtitle_file(input=sample_file, target_language='hu'):
     translated_all = []
 
     for i in range(last_i):
-        a = entries_to_be_translated[start:i + 1]
-        chars_sum = sum([len(t['text']) for t in a])
+        an_entry = entries_to_be_translated[start:i + 1]
+        chars_sum = sum([len(t['text']) for t in an_entry])
         if chars_sum > translation_char_limit - 10 or i == last_i - 1:
             texts = [t['text'] for t in entries_to_be_translated[start:i + 1]]
 
-            time_start: datetime.timedelta = general[entries_to_be_translated[start]['index_end']][1]
-            time_end: datetime.timedelta = general[entries_to_be_translated[i]['index_end']][1]
+            time_start = general[entries_to_be_translated[start]['index_end']][1]
+            time_end = general[entries_to_be_translated[i]['index_end']][1]
 
             # strfdelta(time_start, "{hours}:{minutes}:{seconds}")
             logging.info("Translating {} - {}".format(str(time_start)[:-3], str(time_end)[:-3]))
 
             start = time.time()
-            translated = translate_array(texts=texts, target_language='hu')
+            translated = translate_array(texts=texts, target_language=target_language)
             end = time.time()
             logging.info(
-                "{} requests in {:.2f} seconds,{:.0f} ch/s, {:.2f} req/s".format(len(texts),
-                                                                                                end - start,
-                                                                                                float(chars_sum) / (
-                                                                                                        end - start),
-                                                                                                float(len(texts)) / (
-                                                                                                        end - start)))
+                "{} requests in {:.2f} seconds,{:.0f} ch/s, "
+                "{:.2f} req/s".format(len(texts),
+                                      end - start,
+                                      float(chars_sum) / (
+                                              end - start),
+                                      float(len(texts)) / (
+                                              end - start)))
 
             for res in zip(texts, translated):
                 logging.debug(f" [{res[0]}] -> [{res[1]}]")
@@ -202,11 +182,10 @@ def translate_subtitle_file(input=sample_file, target_language='hu'):
             start = i + 1
 
     logging.info("# Phase 3: Split up sentences (undo #1)")
-    for i in range(len(entries_to_be_translated)):
-        entry = entries_to_be_translated[i]
+    for i, entry in enumerate(entries_to_be_translated):
         text_long = translated_all[i]
         split_pieces = entry['index_end'] - entry['index_start'] + 1
-        texts = split_up(text=text_long, pieces=split_pieces)
+        texts = split_up(text=text_long, pieces_count=split_pieces)
         if len(texts) != split_pieces:
             logging.error("bahh")
 
@@ -221,17 +200,17 @@ def translate_subtitle_file(input=sample_file, target_language='hu'):
         sys.exit(1)
 
     logging.info("# Phase 4: Split up lines")
-    for i in range(len(general)):
-        pieces = int(len(general[i][2]) / 40) + 1
-    if pieces > 1:
-        new_text = "\n".join(split_up(general[i][2], pieces=pieces))
-    general[i][2] = new_text
+    for i, entry in enumerate(general):
+        pieces = int(len(entry[2]) / 40) + 1
+        if pieces > 1:
+            new_text = "\n".join(split_up(entry[2], pieces_count=pieces))
+            entry[2] = new_text
 
     logging.info("# Phase 5: Saving file")
     empty_subtitle = sublib.SubRip()
     empty_subtitle.set_from_general_format(general)
     lines = empty_subtitle.content
-    with open(str(input).replace('.srt', '.out.srt'), 'w', encoding='utf-8') as out:
+    with open(str(input_file).replace('.srt', '.out.srt'), 'w', encoding='utf-8') as out:
         out.writelines(lines)
 
     return None
@@ -242,8 +221,8 @@ if __name__ == "__main__":
 
     # ss = split_up("Ööö, mit csináltál?", 3)
     # ss = split_up("Ööö, mit sdfg sfhg wert sxghsfhgdfhg dfhg g ghdfhg csináltál?", 15)
-    # result = translate_array(texts=["hallo welt", "guten morgen", 'Weltfrieden für Manuela'], target_language='hu')
+    # result = translate_array(texts=["hallo welt", "guten morgen",
+    # 'Weltfrieden für Manuela'], target_language='hu')
 
-    translate_subtitle_file(input=sample_file)
+    translate_subtitle_file(input_file=sample_file)
 
-    pass
