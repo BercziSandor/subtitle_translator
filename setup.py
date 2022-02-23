e51119dd7e5c9bdff56f32975448f48918063a9a
BercziSandor <berczi.sandor@gmail.com>
2022-02-23 15:36:54 +0100

    rwwe

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ __doc__ = __readme__
 
 setuptools.setup(
     name='subtitle_translator',
-    packages=setuptools.find_packages(include=('src',)),
+    # packages=setuptools.find_packages(include=('src',)),
+    py_modules='src/subtitle_translator/main',
 
     long_description=__readme__,
     long_description_content_type='text/markdown',
